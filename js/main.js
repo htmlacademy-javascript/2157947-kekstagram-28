a4ec9f0fc0f813d5b4cd35e8da942a0a5652950f
Kristina Borodina <ty666gh666@mail.ru>
2023-04-09 22:27:16 +0700

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { renderGallerey } from './gallery.js';
 import { getPictures } from './data.js';
+import { showModal } from './form.js';
 const pictures = getPictures;
 
-renderGallerey(pictures());
+renderGallerey(pictures(showModal));
 
